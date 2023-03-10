473408cecc4d3b8a0ec60136def4ae3162a2808b
jin <ellenjin51@gmail.com>
2023-03-10 10:26:49 +0900

    add files

---
+++ modulefile.js
@@ -0,0 +1,51 @@
+const _EXAMDATA = {
+  teamInformation: {
+    teamNumber : 0,
+    teamName: "팀명 작성",
+    teamNameStory: "팀명은 이런이유로 만들었답니다!",
+    teamManager: "공욱재",
+    teamAllMember: [
+      {
+        memberNumber: 0,
+        name: "공욱재",
+        teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "공욱재",
+        teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "공욱재",
+        teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },
+      {
+        memberNumber: 0,
+        name: "공욱재",
+        teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },
+    ]
+  },
+  trainingInformation: {
+    subject : [  
+    "웹프로그래밍기본(HTML5&CSS3)",
+    "Git&GitHub",
+    "Babel Javascript",
+    "프레임워크 기초",
+    "Project C(선택)",
+    "백엔드프레임워크런타임",
+    "프레임워크활용",
+    "Project A(데이터 기반 주식 추천 서비스)",
+    "Project B(교통정보제공서비스)",
+    ]
+  },
+  designInformation: {
+    colorSet : ["cadetblue", "salmon", "#ccc", "#333"],
+    breakPoint : [ "768px", "1280px",  "1920px"]
+  }
+}
