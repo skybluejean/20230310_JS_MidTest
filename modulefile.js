83294cb3d9c772e1cce257b55b01ebf7e7057852
jin <ellenjin51@gmail.com>
2023-03-10 17:31:31 +0900

    move to new branch

--- modulefile.js
+++ modulefile.js
@@ -5,28 +5,35 @@ const _EXAMDATA = {
     teamNameStory: "팀명은 이런이유로 만들었답니다!",
     teamManager: "공욱재",
     teamAllMember: [
+
       {
-        memberNumber: 0,
-        name: "공욱재",
+        memberNumber: 1,
+        name: "박달재",
         teamPosition: "팀장",
+        githubAddress: "https://www.github.com/"
+      },      
+      {
+        memberNumber: 2,
+        name: "이세민",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 3,
+        name: "허진",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 4,
+        name: "이경택",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
       {
-        memberNumber: 0,
-        name: "공욱재",
-        teamPosition: "팀장",
+        memberNumber: 5,
+        name: "정성철",
+        teamPosition: "팀원",
         githubAddress: "https://www.github.com/"
       },
     ]
